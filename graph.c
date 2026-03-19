8055e832c8d940cd91d37ef2efba95dca1b9652c
joaopneves1570 <bolotaneves@gmail.com>
2026-03-19 18:34:50 -0300

    subindo funções f e h

--- graph.c
+++ graph.c
@@ -26,3 +26,52 @@ Graph *MyGraph(int n){
 
     return G;
 }
+
+
+
+
+/* João Neves*/
+
+void print_info(Graph* G){
+    if (G != NULL){
+        int n = G->n;
+        printf("V = [");
+        for (int i = 0; i < n - 1; i++){
+            printf("%d, ", i+1);
+        }
+        printf("%d]\n", n);
+
+        printf("E = [");
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (G->matrix[i][j] != -1){
+                    printf("(%d, %d), ", i+1, j+1);
+                }
+            }
+        }
+        printf("\b\b]\n");
+
+    }
+    return;
+}
+
+int max_neighbors(Graph *G){
+    int vertice = -1;
+    if (G != NULL){
+        int maisVizinhos = 0;
+        int n = G->n;
+        for (int i = n-1; i >= 0; i--){
+            int maisPorLinha = 0;
+            for (int j = n-1; j >= 0; j--){
+                if (G->matrix[i][j] != -1)
+                    maisPorLinha++;
+            }
+            if (maisPorLinha >= maisVizinhos){
+                maisVizinhos = maisPorLinha;
+                vertice = i + 1;
+            }
+        }
+    }
+
+    return vertice;
+}
--- main.c
+++ main.c
@@ -4,7 +4,8 @@ int main(){
     int n; scanf("%d", &n);
     Graph* G = MyGraph(n);
 
-    
+    print_info(G);
+    printf("%d\n", max_neighbors(G));
 
     return 0;
 }
