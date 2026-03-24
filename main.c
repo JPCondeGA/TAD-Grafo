a0747c487807ee9601cc03425b07156a1bb72adc
E-2-B <eduardo2b@usp.br>
2026-03-24 15:46:40 +0000

    Alterando a main.c para ser igual as especificações

--- main.c
+++ main.c
@@ -1,51 +1,46 @@
 #include "graph.h"
 
-/*Recebe um ponteiro para um ponteiro de matriz de inteiros e sua quantidade de linhas n;
-seta para o ponteiro da matriz o valor NULL;
-retorna se a desalocação ocorreu de maneira correta (true, se ocorreu corretamente; false, senão)*/
-bool free_matrix(int ***mat, int n){
-    if(mat == NULL || *mat == NULL) return false;
-    
-    for(int i = 0; i < n; i++){
-        if((*mat)[i] != NULL)
-            free((*mat)[i]);
-    }
-
-    free(*mat);
-    *mat = NULL;
-
-    return true;
-}
-
-//Teste
-
 int main(){
-    int n; scanf("%d", &n);
-    Graph* G = MyGraph(n);
 
-    print_info(G);
-
-    int m; scanf("%d", &m);
-    int x, y, w;
-    for(int i = 0; i < m; i++){
-        scanf("%d %d %d", &x, &y, &w);
-        add_edge(G, x, y, w);
+  int option;
+  int N, x, y, w;
+  int res;
+  bool print_status = 1;
+  
+  Graph *G;
+  
+  scanf("%d", &option);
+  
+  while (option != 1){
+  
+    switch (option){
+    case 0:
+      scanf("%d", &N);
+      G = MyGraph(N);
+      break;
+    case 1:
+      // adicionamos
+      scanf("%d %d %d", &x, &y, &w);
+      add_edge(G, x, y, w);
+      break;
+    case 2:
+      // get
+      scanf("%d %d", &x, &y);
+      res = exist_edge(G, x, y);
+      print_status = 0;
+      break;
+    default:
+      printf("unrecognized option %d!\n", option);
     }
+    scanf("%d\n", &option);
+  }
+  
+  if (option == -1)
+    if (print_status)
+      print_info(G);
+    else
+      printf("%d\n", res);
 
-    int **mat = adjacency_matrix(G);
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            printf("%d ", mat[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("%d\n", max_neighbors(G));
-
-    free_matrix(&mat, n);
-
-    delete_graph(&G);
-
-    return 0;
+  remove_graph(&G);
+  return 0;
 }
